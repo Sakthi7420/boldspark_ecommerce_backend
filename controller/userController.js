5af34be86c27b3ab0b89e25c27124e3db1b7bc5b
Sakthi7420 <Sakthivel.M@gwcdata.ai>
2024-09-13 16:18:47 +0530

    modify registeruser

--- controller/userController.js
+++ controller/userController.js
@@ -1,29 +1,32 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
-
 exports.registerUser = async (req, res) => {
   try {
     const { firstName, lastName, email, password } = req.body;
 
-    if (!firstName || !lastName || !email || !password ) {
-      return res.status(400).json({ message: 'All fields are required' });
-    }
-
+    // Check if user already exists
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: 'User already exists' });
     }
 
-    const user = new User({ firstName, lastName, email, password });
-    await user.save();
-    
+    // Create new user
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password
+    });
+
+    await newUser.save();
     res.status(201).json({ message: 'User registered successfully' });
   } catch (error) {
-    console.error('Registration Error:', error);
-    res.status(500).json({ error: error.message });
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
   }
 };
+
 
 // Login user
 exports.loginUser = async (req, res) => {
